2c7bb295d803b46c3cfa74b8a446abe9e5f46b4a
Jude Collins <jude.collins13@yahoo.com>
2022-01-21 14:45:42 -0500

    serverJS

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
+//  runs express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+require('./routes/htmlRoute')(app);
+require("./routes/apiRoute")(app);
