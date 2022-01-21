a85871c7deded317d5f4871d699c0a936cc32f18
Jude Collins <jude.collins13@yahoo.com>
2022-01-21 14:36:50 -0500

    api routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,2 +1,23 @@
 const fs = require("fs")
 const database = require("../db/db.json")
+
+// creating the delete id
+let id = database.length + 1
+
+module.exports = function (app) {
+
+    // creating api Routes
+    app.get("/api/notes", function (req, res) {
+        res.json(database)
+    })
+    // create post for our api
+    app.post("/api/notes", function (req, res) {
+        req.body.id = id++;
+       
+        database.push(req.body)
+        fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
+            if (err) throw err
+        })
+        res.json(database)
+
+    })
